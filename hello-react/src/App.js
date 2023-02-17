8a67801148b5ae874932786f7d9fb7167455cbc0
Dong Seok Lee <dongseok.lee.log@gmail.com>
2023-02-17 21:31:21 +0900

    2장 JSX까지 함.

---
+++ hello-react/src/App.js
@@ -0,0 +1,23 @@
+// import logo from './logo.svg';
+import './App.css';
+import { Fragment } from 'react';
+
+function App() {
+  const name = '리액트';
+  // const style = {
+  //   //
+  //   backgroundColor: 'black',
+  //   color: 'aqua',
+  //   fontSize: '48px',
+  //   padding: 16
+  // }
+  return (
+    <div className="react">
+      {name}
+      {/* 주석! */}
+      // 주석? adfsfa aefw afdsfdasadsfadfs
+    </div>
+  );
+}
+
+export default App;
