3bc1ff3e43366cc09f3f42a913b86dd44bfb9813
Dong Seok Lee <dongseok.lee.log@gmail.com>
2024-06-04 22:23:20 +0900

    board로 스퀘어를 옮기는 작업 진행중

--- react_official/645e252e-1b46-457f-8f54-7a6092e4b01a/src/App.js
+++ react_official/645e252e-1b46-457f-8f54-7a6092e4b01a/src/App.js
@@ -1,8 +1,23 @@
-function Square({ value }) {
+import { useState } from "react";
+/**
+ * Square 컴포넌트는 게임 보드의 각 칸을 나타냅니다.
+ *
+ * @param {Object} props - 컴포넌트 속성
+ * @param {string} props.value - 버튼에 표시될 값
+ */
+function Square() {
+  const [value, setValue] = useState(null);
+  /**
+   * handleClick 함수는 버튼 클릭 시 호출되며, 콘솔에 "clicked"를 출력합니다.
+   */
   function handleClick() {
-    console.log("clicked");
+    setValue("X");
   }
 
+  /**
+   * 버튼 요소를 반환합니다. 이 버튼은 클릭 시 handleClick 함수를 호출합니다.
+   * 버튼의 내용으로는 props로 전달받은 value 값이 표시됩니다.
+   */
   return (
     <button className="square" onClick={handleClick}>
       {value}
@@ -11,6 +26,7 @@ function Square({ value }) {
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
       <div className="board-row">
