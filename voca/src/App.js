12e8a9f800b1c4a250d7a1604c98b8188f149b30
Dong Seok Lee <dongseok.lee.log@gmail.com>
2024-08-28 21:51:06 +0900

    l3 complete

--- voca/src/App.js
+++ voca/src/App.js
@@ -1,10 +1,23 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const name = "React";
+  const naver = {
+    name: "Naver",
+    url: "https://www.naver.com",
+  };
+
   return (
     <div className="App">
-      <h1>My First React App</h1>
+      <h1
+        style={{
+          color: "blue",
+          backgroundColor: "yellow",
+        }}
+      >
+        Hello {name}! {naver.name} 바
+      </h1>
+      <a href={naver.url}>네이버로 이동</a>
     </div>
   );
 }
