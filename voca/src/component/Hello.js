a60072e0d7e01d246a2a3701dfedd7860624588d
dongseoki <43495319+dongseoki@users.noreply.github.com>
2024-08-31 00:42:45 +0900

    7장 useState 공부 완료.

--- voca/src/component/Hello.js
+++ voca/src/component/Hello.js
@@ -1,22 +1,12 @@
+import { useState } from "react";
 export default function Hello() {
-  function showName() {
-    console.log("Mike");
-  }
-  function showAge(age) {
-    console.log(age);
-  }
-  function showText(txt) {
-    console.log(txt);
-  }
+  const [name, setName] = useState("Micke");
   return (
     <div>
-      <h1>Hello</h1>
-      <button onClick={showName}>Show name </button>
-      <button onClick={() => showAge(10)}>Show age</button>
-      <input type="text" onChange={
-        (e) => showText(e.target.value)
-      } 
-      />
+      <h2 id="name">{name}</h2>
+      <button onClick={() => setName(name === "Mike" ? "Jane":"Mike")}>
+        Change
+      </button>
     </div>
   );
 }
