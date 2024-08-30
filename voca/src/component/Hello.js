075017f1097e81947af29968181e77a146b8ca14
dongseoki <43495319+dongseoki@users.noreply.github.com>
2024-08-31 00:50:40 +0900

    [feat] 7장 프로퍼티 공부 완료.

--- voca/src/component/Hello.js
+++ voca/src/component/Hello.js
@@ -1,9 +1,15 @@
 import { useState } from "react";
-export default function Hello() {
+import UserName from "./UserName";
+export default function Hello({age}) {
   const [name, setName] = useState("Micke");
+  const msg = age > 19 ? "성인입니다." : "미성년자입니다.";
   return (
     <div>
       <h2 id="name">{name}</h2>
+      <h2 id="nage">
+        {name}({age}) : {msg}
+      </h2>
+      <UserName name={name}/>
       <button onClick={() => setName(name === "Mike" ? "Jane":"Mike")}>
         Change
       </button>
