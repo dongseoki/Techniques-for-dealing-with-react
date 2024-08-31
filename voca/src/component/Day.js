af48f5db10114fcfb0a0b80fd11286cb3cea9e8c
Dong Seok Lee <dongseok.lee.log@gmail.com>
2024-08-31 17:26:05 +0900

    11 12? useEffect로 api호출

--- voca/src/component/Day.js
+++ voca/src/component/Day.js
@@ -1,18 +1,30 @@
-import dummy from "../db/data.json";
+// import dummy from "../db/data.json";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Word from "./Word";
 
 export default function Day() {
   // var {id, name} = {id : "id1", name:"name12"}
   const { day } = useParams();
-  const wordList = dummy.words.filter((word) => word.day === Number(day));
+  // const wordList = dummy.words.filter((word) => word.day === Number(day));
+  const [words, setWords] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/words?day=${day}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setWords(data);
+      });
+  }, [day]);
 
   return (
     <>
       <h2>Day {day}</h2>
       <table>
         <tbody>
-          {wordList.map((word) => (
+          {words.map((word) => (
             <Word word={word} key={word.id} />
           ))}
         </tbody>
