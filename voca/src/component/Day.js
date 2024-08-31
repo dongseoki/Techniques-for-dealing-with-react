ae8f9bf37981d00371e3c4c820a3132499fa3102
Dong Seok Lee <dongseok.lee.log@gmail.com>
2024-08-31 17:31:34 +0900

    13 custom hook

--- voca/src/component/Day.js
+++ voca/src/component/Day.js
@@ -1,23 +1,14 @@
 // import dummy from "../db/data.json";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";s
 import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 import Word from "./Word";
 
 export default function Day() {
   // var {id, name} = {id : "id1", name:"name12"}
   const { day } = useParams();
   // const wordList = dummy.words.filter((word) => word.day === Number(day));
-  const [words, setWords] = useState([]);
-
-  useEffect(() => {
-    fetch(`http://localhost:3001/words?day=${day}`)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setWords(data);
-      });
-  }, [day]);
+  const words = useFetch(`http://localhost:3001/words?day=${day}`);
 
   return (
     <>
