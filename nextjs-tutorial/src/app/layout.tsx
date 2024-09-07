83457bdd539985e8025578ec1cfb2fd9bb35bc50
Dong Seok Lee <dongseok.lee.log@gmail.com>
2024-09-07 17:26:58 +0900

    [-ing] 한데까지 일단 저장..

--- nextjs-tutorial/src/app/layout.tsx
+++ nextjs-tutorial/src/app/layout.tsx
@@ -1,17 +1,20 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
 import "./globals.css";
+import 'semantic-ui-css/semantic.min.css'
+import Top from "../component/Top";
+import Footer from "../component/Footer";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,10 +28,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
+      {/* <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+      </body> */}
+      <body>
+        <div style={{ width: 1000, margin: "0 auto" }}>
+          <Top />
+          {children}
+          <Footer />
+        </div>      
       </body>
     </html>
   );
