f24308421e6d425e1db07a2962faa23896fab9fd
이동석 ezpmp 연구원 <dslee@ezpmp.co.kr>
2024-09-13 12:45:40 +0900

    일단 커밋 tsx 전환중

--- voca/src/component/CreateWord.tsx
+++ voca/src/component/CreateWord.tsx
@@ -1,61 +1,75 @@
-import {useRef} from "react";
-import {useNavigate} from "react-router-dom";
+import React, { useRef, useState } from "react";
+import { useHistory } from "react-router";
 import useFetch from "../hooks/useFetch";
-import {useState} from "react";
+import { IDay } from "./DayList";
 
 export default function CreateWord() {
-    const days = useFetch("http://localhost:3001/days");
-    const history = useNavigate();
-    const [isLoading, setIsLoading] = useState(false);
-    function onSubmit(e) {
-        e.preventDefault();
-        if (!isLoading) {
-            setIsLoading(true);
-        }
-        fetch(`http://localhost:3001/words/`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                day: dayRef.current.value,
-                eng: engRef.current.value,
-                kor: korRef.current.value,
-                isDone: false,
-            }),
-        }).then((res) => {
-            if (res.ok) {
-                alert("생성이 완료되었습니다.");
-                history(`/day/${dayRef.current.value}`);
-                setIsLoading(false);
-            }
-        });
-    }
+  const days: IDay[] = useFetch("http://localhost:3001/days");
+  const history = useHistory();
+  const [isLoading, setIsLoading] = useState(false);
 
-    const engRef = useRef(null);
-    const korRef = useRef(null);
-    const dayRef = useRef(null);
-    return (
-        <form onSubmit={onSubmit}>
-            <div>
-                <div>
-                    <label>Eng</label>
-                    <input type="text" placeholder="computer" ref={engRef}/>
-                </div>
-                <div>
-                    <label>Kor</label>
-                    <input type="text" placeholder="컴퓨터" ref={korRef}/>
-                </div>
-                <div>
-                    <label>Day</label>
-                    <select ref={dayRef}>
-                        {days.map(day => {
-                            return <option key={day.id} value={day.day}>{day.day}</option>
-                        })}
-                    </select>
-                </div>
-            </div>
-            <button>저장</button>
-        </form>
-    );
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    if (!isLoading && dayRef.current && engRef.current && korRef.current) {
+      setIsLoading(true);
+
+      const day = dayRef.current.value;
+      const eng = engRef.current.value;
+      const kor = korRef.current.value;
+
+      fetch(`http://localhost:3001/words/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          day,
+          eng,
+          kor,
+          isDone: false,
+        }),
+      }).then(res => {
+        if (res.ok) {
+          alert("생성이 완료 되었습니다");
+          history.push(`/day/${day}`);
+          setIsLoading(false);
+        }
+      });
+    }
+  }
+
+  const engRef = useRef<HTMLInputElement>(null);
+  const korRef = useRef<HTMLInputElement>(null);
+  const dayRef = useRef<HTMLSelectElement>(null);
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div className="input_area">
+        <label>Eng</label>
+        <input type="text" placeholder="computer" ref={engRef} />
+      </div>
+      <div className="input_area">
+        <label>Kor</label>
+        <input type="text" placeholder="컴퓨터" ref={korRef} />
+      </div>
+      <div className="input_area">
+        <label>Day</label>
+        <select ref={dayRef}>
+          {days.map(day => (
+            <option key={day.id} value={day.day}>
+              {day.day}
+            </option>
+          ))}
+        </select>
+      </div>
+      <button
+        style={{
+          opacity: isLoading ? 0.3 : 1,
+        }}
+      >
+        {isLoading ? "Saving..." : "저장"}
+      </button>
+    </form>
+  );
 }
